a4fc5b84a4c7f01dc20224ce7c7a4cbf71275899
rafiazisrachman <149140188+rafiazisrachman@users.noreply.github.com>
2023-11-16 23:48:11 +0700

    Update Hurdle3.py

--- tugas 2/Hurdle3.py
+++ tugas 2/Hurdle3.py
@@ -1,1 +1,19 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
+def pass_wall():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+    
+while at_goal() == False:
+    if wall_in_front():
+        pass_wall()
+    elif not wall_in_front():
+        move()
