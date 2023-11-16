07eb0a6725f724482bb97b1f825b7ac8bedfee29
Jocellinakei <149289714+jocellinakei@users.noreply.github.com>
2023-11-16 20:08:25 +0700

    Create Hurdle1.py

---
+++ Hurdle1.py
@@ -0,0 +1,23 @@
+def walk():
+  turn_left()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  turn_left()
+
+def jump():
+  move()
+  walk()
+
+jump()
+jump()
+jump()
+jump()
+jump()
+jump()
