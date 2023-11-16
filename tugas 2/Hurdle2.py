5c3c140e1173dab601f8a7f5f6fc3782991e2bc4
AchmadZulfahmi <149311690+AchZulfahmi@users.noreply.github.com>
2023-11-16 23:54:05 +0700

    Update Hurdle2.py

--- tugas 2/Hurdle2.py
+++ tugas 2/Hurdle2.py
@@ -1,1 +1,19 @@
-
+def walk():
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    
+def jump():
+    move()
+    walk()
+    
+while not at_goal():
+    jump()
