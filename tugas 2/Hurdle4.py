abf5dd3935ef1b57ff55a2829dcbe3aefd964ba3
Jocellinakei <149289714+jocellinakei@users.noreply.github.com>
2023-11-16 23:55:06 +0700

    Update Hurdle4.py

--- tugas 2/Hurdle4.py
+++ tugas 2/Hurdle4.py
@@ -2,3 +2,16 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
+def pass_wall():
+    turn_left()
+    count_wall = 0
+    while wall_on_right():
+        count_wall+=1
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while count_wall >0:
+        move()
+        count_wall-=1
+    turn_left()
